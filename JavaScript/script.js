d9e7ab05edc72259b6903343dc4ee20337016263
HashirNaqvi <108654560+HashirNaqvi@users.noreply.github.com>
2024-05-20 21:27:46 +0500

    assignment 4

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,12 @@
-for (let i = 1; i <= 10; i++) {
-  console.log(`2 x ${i} = ${2 * i}`);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`2 x ${i} = ${2 * i}`);
+// }
+
+// Assignment 4
+function table(num1, num2) {
+  for (let i = 1; i <= num2; i++) {
+    console.log(`${num1}x${i} = ${num1 * i}`);
+  }
 }
+
+let x = table(18, 45);
